d7f5995dcc6820014acc6acc50b5f478f109d7de
1gtm <1gtm@appscode.com>
2018-08-10 20:25:33 +0600

    Brew formula update for gh-tools-darwin-amd64 version

--- gh-tools.rb
+++ gh-tools.rb
@@ -1,8 +1,8 @@
 class GhTools < Formula
   desc ""
   homepage "https://appscode.com"
-  url "https://github.com/appscodelabs/gh-tools/releases/download/0.0.1/gh-tools-darwin-amd64"
-  version "0.0.1"
+  url "https://github.com/appscodelabs/gh-tools/releases/download//gh-tools-darwin-amd64"
+  version ""
   sha256 "59b383964406ccae3214834145c3ee4e739a992b63c94d1028d966dd31535f7e"
 
   def install
